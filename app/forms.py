f5d679de778b55f6b436d2cbd1b68aef41a561ba
Carol <carolineferreirap@gmail.com>
2021-05-15 02:56:01 -0300

    arrumando os bugs

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,13 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
 from app.models import Usuarios
 
 # Create the form class.
 class UsuariosForm(ModelForm):
+  password = forms.CharField(widget=forms.PasswordInput())
   class Meta:
     model = Usuarios
-    fields = ['nome', 'email', 'cpf', 'tipo', 'password']
+    fields = ['nome', 'email', 'cpf', 'password']
     # widgets = {
     #   'password': forms.PasswordInput(),
     # }
-
-class LoginForm(ModelForm):
-  class Meta:
-    model = Usuarios
-    fields = ['email', 'password']
--- app/views.py
+++ app/views.py
@@ -1,41 +1,38 @@
 from django.shortcuts import render, redirect
-from app.forms import UsuariosForm, LoginForm
+from app.forms import UsuariosForm
 from app.models import Usuarios
 from django.core.paginator import Paginator
 
 # Create your views here.
+def login(request):
+  if request.method == "POST":
+    data = UsuariosForm(request.POST)
+    user = (data["email"].value())
+    senha = (data["password"].value())
+    return render(request, 'form.html')
+  else:
+    data = {}
+    data['form'] = UsuariosForm()
+    return render(request, 'login.html', data)
+
 def home(request):
   data = {}
   data['form'] = UsuariosForm()
-  # search = request.GET.get('search')
-  # if search:
-  #   data['db'] = Usuarios.objects.filter(nome__icontains=search)
-  # else:
-  #   data['db'] = Usuarios.objects.all()
-  # all = Usuarios.objects.all()
-  # paginator = Paginator(all, 2)
-  # pages = request.GET.get('page')
-  # data['db'] = paginator.get_page(pages)
+  search = request.GET.get('search')
+  if search:
+    data['db'] = Usuarios.objects.filter(nome__icontains=search)
+  else:
+    data['db'] = Usuarios.objects.all()
+  all = Usuarios.objects.all()
+  paginator = Paginator(all, 2)
+  pages = request.GET.get('page')
+  data['db'] = paginator.get_page(pages)
   return render(request, 'index.html', data)
 
 def form(request):
   data = {}
   data['form'] = UsuariosForm()
   return render(request, 'form.html', data)
-
-def login(request):
-  data = {}
-  data['form'] = LoginForm()
-  return render(request, 'login.html', data)
-  # if request.method == "POST":
-  #   data = UsuariosForm(request.POST)
-  #   user = (data["email"].value())
-  #   senha = (data["password"].value())
-  #   return render(request, 'form.html')
-  # else:
-  #   data = {}
-  #   data['form'] = UsuariosForm()
-  #   return render(request, 'index.html', data)
 
 def create(request):
   form = UsuariosForm(request.POST or None)
